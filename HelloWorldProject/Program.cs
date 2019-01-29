5b40372543ecedc0ecc6d3aade4bcf67fffc87e9
Chris Caudill <christopherscaudill@gmail.com>
2019-01-29 16:31:33 -0500

    inital commit

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -5,11 +5,40 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloWorldProject
+    /*
+     * This is a muti-line comment
+     */
 {
-    class Program
+    class Program   //This is a class
     {
-        static void Main(string[] args)
+        static int a = 7;
+        static void WhileExample()
         {
+            var counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Count is {counter}");
+                counter = counter + 1;
+            }
+        }
+        static void Print()
+        {
+            Console.WriteLine($"The value of a is {a}");
+        }
+        static void Main(string[] args) //This is a method
+        {
+            WhileExample();
+            Print();
+            var counter = 0;
+            //var a = 7;
+            var b = 13;
+            var c = a * b;
+            Console.WriteLine("Hello, World!"); //This is a statement, Console is a class in namespace
+            Console.WriteLine("By Chris");
+            Console.WriteLine("Another line.");
+            Console.WriteLine($"For a = {a} and b = {b}, A*B = {c}");
+            Console.WriteLine($"Value of counter is {counter}");
+            Console.WriteLine("Value of counter is " + counter); //dont use it this way, this is old way
         }
     }
 }
